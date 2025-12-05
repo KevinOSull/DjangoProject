644f5fa20ffe57457c57f2b4e32d235c83d4b7a9
kevin o sullivan <kosullivan3336@gmail.com>
2025-12-05 12:43:49 +0000

    added superuser and set up database

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL",
+    "postgresql://neondb_owner:npg_jmfgVcyL5M8w@ep-purple-haze-agn9a06e.c-2.eu-central-1.aws.neon.tech/raft_quake_april_36951"
+)
