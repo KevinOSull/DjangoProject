efbdf2ecd7993a6e8adf9b8d30e259a988ac58da
kevin o sullivan <kosullivan3336@gmail.com>
2025-12-06 12:33:41 +0000

    added secret key to env file

--- env.py
+++ env.py
@@ -4,3 +4,5 @@ os.environ.setdefault(
     "DATABASE_URL",
     "postgresql://neondb_owner:npg_jmfgVcyL5M8w@ep-purple-haze-agn9a06e.c-2.eu-central-1.aws.neon.tech/raft_quake_april_36951"
 )
+
+os.environ.setdefault("SECRET_KEY", 'django-insecure-1(1@)#g!r_vl58w6o$q+&p!r@(mzx+t8ciiqg1rbrxv!mlrgw6')
