55d664a5b841bcaabe33f236a5002decf0d7f278
kevin o sullivan <kosullivan3336@gmail.com>
2025-12-10 15:56:58 +0000

    debugging internal server error

--- game/urls.py
+++ game/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.GameListView.as_view(), name='home'),
-    path('<slug:slug>/', views.GameDetailView.as_view(), name='game_detail'),
+    path('<slug:slug>/', views.game_detail.as_view(), name='game_detail'),
 ]
