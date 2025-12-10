2f85baa4eb10277032ccceba909d9cc9703dfef1
kevin o sullivan <kosullivan3336@gmail.com>
2025-12-10 18:10:09 +0000

    added code so slug field automatically fills

--- game/admin.py
+++ game/admin.py
@@ -1,9 +1,29 @@
+'''
 from django.contrib import admin
 from .models import Game, Comment
 # Register your models here.
 
 class GameAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Game, GameAdmin)
+admin.site.register(Comment)
+'''
+
+from django.contrib import admin
+from django.utils.text import slugify
+from .models import Game, Comment  
+
+@admin.action(description='Generate slugs for selected games')
+def generate_slugs(modeladmin, request, queryset):
+    for game in queryset:
+        if not game.slug:
+            game.slug = slugify(game.name)
+            game.save()
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    actions = [generate_slugs]
 
 admin.site.register(Game, GameAdmin)
 admin.site.register(Comment)
