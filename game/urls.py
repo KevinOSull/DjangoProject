4fa60792e92f94330b798c3a9f04d7da12099525
kevin o sullivan <kosullivan3336@gmail.com>
2025-12-10 15:49:12 +0000

    added html and image

--- game/urls.py
+++ game/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.GameListView.as_view(), name='home'),
+    path('<slug:slug>/', views.GameDetailView.as_view(), name='game_detail'),
 ]
--- game/views.py
+++ game/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Game, Comment
 
@@ -8,4 +8,8 @@ class GameListView(generic.ListView):
     queryset = Game.objects.all()
     template_name = "game/index.html"
     paginate_by = 6
+
+def game_detail(request,slug):
+    game = get_object_or_404(Game,slug=slug)
+    return render(request, 'game/game_post.html', {'game': game},)
 
