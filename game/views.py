c73f7ae7fe86adc983749e4920246cb4b29396df
kevin o sullivan <kosullivan3336@gmail.com>
2025-12-11 16:12:04 +0000

    added comments text area

--- game/views.py
+++ game/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Game, Comment
-
+from .forms import CommentForm
 # Create your views here.
 class GameListView(generic.ListView):
     #model = Game
@@ -9,7 +10,32 @@ class GameListView(generic.ListView):
     template_name = "game/index.html"
     paginate_by = 6
 
-def game_detail(request,slug):
-    game = get_object_or_404(Game,slug=slug)
-    return render(request, 'game/game_post.html', {'game': game},)
+def game_detail(request, slug):
+    game = get_object_or_404(Game, slug=slug)
+    user_comments = game.comments.filter(user=request.user)
+    comment_count = user_comments.count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.user = request.user     # match your Comment model
+            comment.game = game             # match your Comment model
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted '
+            )
+            # reload comments after saving
+            user_comments = game.comments.filter(user=request.user)
+            comment_count = user_comments.count()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'game/game_post.html', {
+        'game': game,
+        'comments': user_comments,
+        'comment_count': comment_count,
+        'comment_form': comment_form
+    })
 
